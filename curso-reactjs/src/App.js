5b6f062c0d4a9a404c70663a80be147b3b3a5ed6
Claudivan Moreira <claudivan.a.moreira@accenture.com>
2018-11-02 20:47:43 -0300

    [Curso ReactJS]

---
+++ curso-reactjs/src/App.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+import React, { Component } from 'react';
+
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
