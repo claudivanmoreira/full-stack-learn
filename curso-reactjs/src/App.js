1ce61c82d4b0a62d0d92b4d3691758dfd99f0a18
Claudivan Moreira <claudivan.a.moreira@accenture.com>
2018-11-04 15:52:39 -0200

    [Curso ReactJS]

--- curso-reactjs/src/App.js
+++ curso-reactjs/src/App.js
@@ -3,12 +3,15 @@ import './style.css'
 import React, { Component } from 'react';
 
 import Header from './components/Header';
+import Main from './pages/main';
+import Routes from './routers';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Routes />
       </div>
     );
   }
